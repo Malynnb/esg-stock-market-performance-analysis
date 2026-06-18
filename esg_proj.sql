aece7de9a7218762bf2770f7e63244a418f0946e
Malynn Buranawichian <thisismalynn@gmail.com>
2026-06-18 10:59:33 +0200

    Add files via upload

--- esg_proj.sql
+++ esg_proj.sql
@@ -60,9 +60,66 @@ LIMIT 10;
 
 
 
-# Imported enriched ESG dataset into MySQL
-# Used SQL for aggregation, ranking, and business analysis
-# Validated ESG group performance and subindustry trends
-# Queried API-enriched market data
+
+DESCRIBE enriched_esg_dataset;
+
+-- top return co
+SELECT Company_name,
+       Symbol,
+       Company_ESG_pulse,
+       `52w_change price percent` AS return_pct
+FROM enriched_esg_dataset
+ORDER BY `52w_change price percent` DESC
+LIMIT 10;
+
+-- lowest return co
+SELECT Company_name,
+       Symbol,
+       Company_ESG_pulse,
+       `52w_change price percent` AS return_pct
+FROM enriched_esg_dataset
+ORDER BY `52w_change price percent` ASC
+LIMIT 10;
 
 
+
+-- business questions
+-- How are ESG scores distributed across industrial companies?
+SELECT ESG_Group,
+       COUNT(*) AS companies
+FROM enriched_esg_dataset
+GROUP BY ESG_Group;
+
+-- Do high ESG companies achieve stronger stock returns?
+SELECT ESG_Group,
+       AVG(`52w_change price percent`) AS avg_return
+FROM enriched_esg_dataset
+GROUP BY ESG_Group;
+
+-- Which subindustries demonstrate the strongest ESG performance?
+SELECT gicSubindustry,
+       AVG(Company_ESG_pulse) AS avg_esg
+FROM enriched_esg_dataset
+GROUP BY gicSubindustry
+ORDER BY avg_esg DESC;
+
+
+
+
+
+-- Key SQL Insights:
+-- Most companies fall within the Medium ESG category
+-- High ESG companies achieved stronger average 52-week returns
+-- ESG performance varies across subindustries
+-- Company-level analysis revealed differences in financial performance among firms with similar ESG ratings
+
+
+
+--  SUMMARY
+-- Dataset exploration and validation
+-- ESG distribution analysis
+-- ESG group performance comparison
+-- Subindustry benchmarking
+-- ompany-level analysis
+-- API data validation
+-- Dashboard support queries
